cf9405110bf70229e5ed17f16b14c15dd93ac7c7
marco <m.mannucci@solvingteam.it>
2010-02-22 21:58:55 +0100

    modifica al controller per parametri di request

--- src/java/it/prova/controller/EditoreController.java
+++ src/java/it/prova/controller/EditoreController.java
@@ -24,21 +24,26 @@ public class EditoreController {
 
 	@RequestMapping(value = {"/editore.dispatch", "/editore/list.dispatch"}, method = RequestMethod.GET)
 	public String list(@RequestParam(value = "offset", required = false) Integer offset, @RequestParam(value = "max", required = false) Integer max,
-			@RequestParam(value = "sort", required = false) String sort, @RequestParam(value = "order", required = false) String order, ModelMap modelMap) {
+			@RequestParam(value = "sort", required = false) String sort, @RequestParam(value = "order", required = false) String order, HttpServletRequest request, ModelMap modelMap) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.ACTION_NAME_ATTRIBUTE, "list.dispatch");
 		int sizeNo = max == null ? 10 : max.intValue();
 		modelMap.addAttribute("editoreInstanceList", Editore.findAll(offset == null ? 0 : (offset.intValue() - 1), sizeNo, sort, order));
 		modelMap.addAttribute("editoreInstanceTotal", Editore.count());
+		
 		return "/editore/list";
 	}
 
 	@RequestMapping(value = "/editore/create.dispatch", method = RequestMethod.GET)
-	public String create(ModelMap modelMap) {
+	public String create(HttpServletRequest request, ModelMap modelMap) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		modelMap.addAttribute("editoreInstance", new Editore());
 		return "/editore/create";
 	}
 
 	@RequestMapping(value = "/editore/save.dispatch", method = RequestMethod.POST)
 	public String save(HttpServletRequest request, ModelMap modelMap) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		Editore editore = Editore.create();
 		MyUtils.bindDataFromMap(editore, request);
 		if (!editore.validate()) {
@@ -46,18 +51,21 @@ public class EditoreController {
 			return "/editore/create";
 		}        
 		Long id = editore.save();
+
 		return "redirect:/editore/" + id + ".dispatch";
 	}
 
 	@RequestMapping(value = {"/editore/{id}.dispatch", "/editore/show/{id}.dispatch"}, method = RequestMethod.GET)
-	public String show(@PathVariable("id") Long id, ModelMap modelMap) {
+	public String show(@PathVariable("id") Long id, HttpServletRequest request, ModelMap modelMap) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		if (id == null) throw new IllegalArgumentException("An Identifier is required");
 		modelMap.addAttribute("editoreInstance", Editore.get(id));
 		return "/editore/show";
 	}
 
 	@RequestMapping(value = "/editore/edit/{id}.dispatch", method = RequestMethod.GET)
-	public String edit(@PathVariable("id") Long id, ModelMap modelMap) {
+	public String edit(@PathVariable("id") Long id, HttpServletRequest request, ModelMap modelMap) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		if (id == null) throw new IllegalArgumentException("An Identifier is required");
 		modelMap.addAttribute("editoreInstance", Editore.get(id));
 		return "/editore/edit";
@@ -65,7 +73,7 @@ public class EditoreController {
 
 	@RequestMapping(value = "/editore/update.dispatch", method = RequestMethod.POST)
 	public String update(@RequestParam("id") Long id, HttpServletRequest request, ModelMap modelMap) {
-
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		Editore editore = Editore.get(id);
 		if (editore == null) throw new IllegalArgumentException("A editore is required");
 		MyUtils.bindDataFromMap(editore, request);
@@ -78,7 +86,8 @@ public class EditoreController {
 	}
 
 	@RequestMapping(value = "/editore/delete/{id}.dispatch", method = RequestMethod.GET)
-	public String delete(@PathVariable("id") Long id, @RequestParam(value = "offset", required = false) Integer offset, @RequestParam(value = "max", required = false) Integer max) {
+	public String delete(@PathVariable("id") Long id, @RequestParam(value = "offset", required = false) Integer offset, @RequestParam(value = "max", required = false) Integer max, HttpServletRequest request) {
+		request.setAttribute(org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE, "editore");
 		if (id == null) throw new IllegalArgumentException("An Identifier is required");
 		Editore.get(id).delete();
 		return "redirect:/editore.dispatch?offset=" + ((offset == null) ? "1" : offset.toString()) + "&max=" + ((max == null) ? "10" : max.toString());
