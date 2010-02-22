140dddfd10d274361465d29bba8119dc5752efea
marco <m.mannucci@solvingteam.it>
2010-02-22 22:00:01 +0100

    modificato namespace taglib di navigazione

--- grails-app/taglib/it/solvingteam/taglib/NavigationTagLib.groovy
+++ grails-app/taglib/it/solvingteam/taglib/NavigationTagLib.groovy
@@ -7,11 +7,11 @@ import org.springframework.context.ApplicationContext;
 
 
 class NavigationTagLib extends ApplicationTagLib {
-	
-	def stLink = { attrs, body ->
+	static namespace = 'st'
+	def link = { attrs, body ->
 		def writer = getOut()
 		def elementId = attrs.remove('elementId')
-		writer <<  "<a href=\"${createStLink(attrs).encodeAsHTML()}\""
+		writer <<  "<a href=\"${createLink(attrs).encodeAsHTML()}\""
 		if(elementId) {
 			writer << " id=\"${elementId}\""
 		}
@@ -20,7 +20,7 @@ class NavigationTagLib extends ApplicationTagLib {
 		writer << "${body()}</a>"
 	}
 	
-	def createStLink = { attrs ->
+	def createLink = { attrs ->
 		def writer = getOut()
 		// prefer URI attribute
 		if(attrs['uri']) {
