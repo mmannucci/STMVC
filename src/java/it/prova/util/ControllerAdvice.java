dac2864d2bb3e5072add8972f53fb4c9b6254cae
marco <m.mannucci@solvingteam.it>
2010-02-24 21:11:32 +0100

    ControllerAdvice

---
+++ src/java/it/prova/util/ControllerAdvice.java
@@ -0,0 +1,26 @@
+package it.prova.util;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.codehaus.groovy.grails.web.servlet.GrailsApplicationAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+
+@Component
+@Aspect
+public class ControllerAdvice {
+	@Before("within(*..*Controller)")
+	public void setRequestParameters(JoinPoint pjp) {
+		Signature sig = pjp.getSignature();
+		
+		String declaringTypeName = sig.getDeclaringTypeName(); 
+		String controllerName = declaringTypeName.substring(0, declaringTypeName.indexOf("Controller")).toLowerCase();
+		
+		String actionName = sig.getName();
+		RequestContextHolder.currentRequestAttributes().setAttribute(GrailsApplicationAttributes.CONTROLLER_NAME_ATTRIBUTE,controllerName, RequestAttributes.SCOPE_REQUEST);
+		RequestContextHolder.currentRequestAttributes().setAttribute(GrailsApplicationAttributes.ACTION_NAME_ATTRIBUTE, actionName, RequestAttributes.SCOPE_REQUEST);
+	}
+}
